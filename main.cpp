d34fc7b40f466fd7bddedfb2a7a3d61f1d1548b4
dawidsanecki <dawid.aleksander.sanecki@estudiantat.upc.edu>
2021-06-04 01:05:45 +0200

    Simulacion empezada

--- main.cpp
+++ main.cpp
@@ -1,19 +1,120 @@
 #include "iostream"
+#include "vector"
+#include "list"
 using namespace std;
 
-void SimulationWithoutChange(){
+
+//Clase cliente
+class Cliente {
+    int TiempoEntrada;
+    int TiempoSalida;
+public:
+
+    void SetTiempoEntrada(int ticks){
+        TiempoEntrada = ticks;
+    }
+    void GuardarTiempoFinal(int ticks) {
+        TiempoSalida = ticks - TiempoEntrada;
+    }
+    int GetTiempoSalida(){
+        return TiempoSalida;
+    }
+    bool WantsChange(int ticks,int tiempoEspera){
+        if(ticks - TiempoEntrada >= tiempoEspera) return true;
+        else return false;
+    }
+};
+
+class Cajero {
+
+public:
+    vector<Cliente> cola;
+
+    void EntrarCola(Cliente c){
+        cola.push_back(c);
+    }
+
+    Cliente SalirCola(){
+        Cliente c = cola[0];
+        cola.erase(cola.begin());
+        return c;
+    }
+
+    void CambiarCola(Cajero &CajeroDestino,int PosCliente){
+        Cliente c = cola[PosCliente];
+        cola.erase(cola.begin()+PosCliente);
+        CajeroDestino.EntrarCola(c);
+    }
+
+    int GetLength(){
+        return cola.size();
+    }
+};
+
+Cajero CompararColas(Cajero &c1, Cajero &c2, Cajero &c3){
+    int cl1 = c1.GetLength();
+    int cl2 = c2.GetLength();
+    int cl3 = c3.GetLength();
+    //3 colas iguales
+    if(cl1 == cl2 && cl2 == cl3){
+        int numr = rand() % 3;
+        if(numr == 0) return c1;
+        else if(numr == 1) return c2;
+        else return c3;
+    }
+    //2 colas iguales
+    else if(cl1 == cl2 && cl1 < cl3){
+        int numr = rand() % 2;
+        if(numr == 0) return c1;
+        else return c2;
+    }
+    else if(cl1 == cl2 && cl1 > cl3) return c3;
+
+    else if(cl1 == cl3 && cl1 < cl2){
+        int numr = rand() % 2;
+        if(numr == 0) return c1;
+        else return c3;
+    }
+    else if(cl1 == cl3 && cl1 > cl2) return c2;
+
+    else if(cl2 == cl3 && cl1 > cl2){
+        int numr = rand() % 2;
+        if(numr == 0) return c2;
+        else return c3;
+    }
+    else if(cl2 == cl3 && cl1 < cl2) return c1;
+
+    //3 cola diferentes
+    else if(cl1 < cl2 && cl1 < cl3) return c1;
+    else if(cl2 < cl3 && cl2 < cl1) return c2;
+    else if(cl3 < cl1 && cl3 < cl2) return c3;
+
+}
+
+void SimulationWithoutChange(int IntervalLLegada, int SeedCajero1, int SeedCajero2, int SeedCajero3){
 
     int ticks = 0; //Ticks son nuestro metodo de medir el tiempo, cada tick representa un minuto
+    vector<Cliente> ClientesSalida; //Clientes que salen de la simulacion
+    Cajero c1;
+    Cajero c2;
+    Cajero c3;
     while(ticks < 720){
-        ticks++;
+        //Entrada nuevo cliente cada intervalo
+        if(ticks % IntervalLLegada == 0){
+            Cliente c;
+            c.SetTiempoEntrada(ticks);
+            CompararColas(c1,c2,c3).EntrarCola(c);
+        }
 
+        ticks++;
     }
 
 }
 
-void SimulationWithChange(int MinutosHastaCambio, double ProbabilidadDeCambio){
+void SimulationWithChange(int IntervalLLegada,int MinutosHastaCambio, double ProbabilidadDeCambio, int SeedCajero1, int SeedCajero2, int SeedCajero3){
 
     int ticks = 0; //Ticks son nuestro metodo de medir el tiempo, cada tick representa un minuto
+    vector<Cliente> ClientesSalida; //Clientes que salen de la simulacion
     while(ticks < 720){
         ticks++;
 
@@ -26,10 +127,10 @@ void ScenarioExecute(){
     cout << "Escoge uno de los 2 escenarios" << endl << "1.3 cajeros con colas individuales" << endl << "2.3 cajeros con colas individuales pero los clientes pueden cambiar de cola despues de estar 15 minutos en una" << endl;
     cin >> escenario;
     if(escenario == 1){
-        SimulationWithoutChange();
+        SimulationWithoutChange(8,5234,234234,25332);
     }
     else if(escenario == 2){
-        SimulationWithChange();
+        SimulationWithChange(8,15,0.15,23423,9035,8394);
     }
 }
 
@@ -44,10 +145,36 @@ void DataImport(){
         cout << "Que probabilidad de cambio quieres asignar? (0 - 1)" << endl;
         double ProbabilidadCambio;
         cin >> ProbabilidadCambio;
-        SimulationWithChange(TiempoCambio, ProbabilidadCambio);
+        cout << "Cada cuantos minutos llega una persona" << endl;
+        int IntervalLlegada;
+        cin >> IntervalLlegada;
+        cout << "Indica un seed para el cajero 1 (numero entero)" << endl;
+        int s1;
+        cin >> s1;
+        cout << "Indica un seed para el cajero 2 (numero entero)" << endl;
+        int s2;
+        cin >> s2;
+        cout << "Indica un seed para el cajero 3 (numero entero)" << endl;
+        int s3;
+        cin >> s3;
+        SimulationWithChange( IntervalLlegada,TiempoCambio, ProbabilidadCambio,s1,s2,s3);
     }
     else if(ans == "n"){
-        SimulationWithoutChange();
+        cout << "Cada cuantos minutos llega una persona" << endl;
+        int IntervalLlegada;
+        cin >> IntervalLlegada;
+        cout << "Indica un seed para el cajero 1 (numero entero)" << endl;
+        int s1;
+        cin >> s1;
+        cout << "Indica un seed para el cajero 2 (numero entero)" << endl;
+        int s2;
+        cin >> s2;
+        cout << "Indica un seed para el cajero 3 (numero entero)" << endl;
+        int s3;
+        cin >> s3;
+
+
+        SimulationWithoutChange(IntervalLlegada,s1,s2,s3);
     }
 }
 
